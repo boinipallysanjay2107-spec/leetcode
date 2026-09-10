e3cfa09a174ffe2e4c6921c0ffa2993d4e1c180a
Boinipally sanjay <boinipallysanjay2107@gmail.com>
2026-09-10 21:54:51 +0530

    Time: 15 ms (34.02%) | Memory: 32.2 MB (21.26%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -5,7 +5,7 @@ public:
        int n=nums.size();
        int ans=0;
        for(int i=0;i<n;i=i+2){
-        ans=ans+nums[i];
+        ans=ans+min(nums[i],nums[i+1]);
        }
        return ans;
     }
